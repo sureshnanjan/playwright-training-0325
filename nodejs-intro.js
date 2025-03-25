97e3d3cf86b5597189fc13428dcfc073685cec1d
Nitish Kumar - GE-BC <Nitish.8.Kumar@coforge.com>
2025-03-25 22:30:59 +0530

    Intoduction_to_Playwright

--- nodejs-intro.js
+++ nodejs-intro.js
@@ -6,7 +6,7 @@ const port = 3000;
 const server = createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello Welcome to Playwright Training');
+  res.end('Hello Nitish , Welcome to Playwright Training');
 });
 
 server.listen(port, hostname, () => {
