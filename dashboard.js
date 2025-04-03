2b891b447e7ecf8587b6ac4297af106a2e8061d1
sureshnanjan <sureshnanjan@outlook.com>
2025-04-03 21:17:43 +0530

    Identifiers Examle

---
+++ dashboard.js
@@ -0,0 +1,28 @@
+import { type } from "os"
+
+console.log("Training Dashboard")
+// suresh id trainig - single 
+/* multilie commet is spearad acr
+diffeet line */
+// let , var , const
+let name1 = "Playwrigt Training"
+let trainer = "suresh"
+let no_of_particpants = 18
+let participants = ["chris", "aditya"]
+let ApiResponse = {
+    code : 12,
+    response : "OK",
+    message: "200 OK"
+}
+let completed = false
+
+console.log(typeof name1)
+console.log(typeof trainer)
+console.log(typeof no_of_particpants)
+console.log(typeof participants)
+console.log(typeof completed)
+console.log(typeof ApiResponse)
+console
+console.log(name1.toUpperCase())
+console.log(trainer.toUpperCase())
+console.log(ApiResponse)
