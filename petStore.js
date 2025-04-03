076d90b6c5b2fc48474888b1ca4f7b19efda4204
Shubham Shree Mahapatra <Shubham.Mahapatra@coforge.com>
2025-04-03 21:51:36 +0530

    petstore main update is stored

---
+++ petStore.js
@@ -0,0 +1,26 @@
+const pet = {
+    id: 1,
+    category: {
+      id: 1,
+      name: "rotwiller"
+    },
+    name: "Rocky_doggie",
+    photo: [
+      "string","string"
+    ],
+    tags: [
+      {
+        id: 10,
+        name: "bhoku"
+      }
+    ],
+    status: "available"
+  };
+
+  console.log("pet id : "+ pet.id)
+  console.log("Pet categroy id "+ pet.category.id)
+  console.log("Pet category name"+pet.category.name)
+  console.log("pet name"+pet.name)
+  console.log("pet tags"+pet.tags)
+  console.log("pet tags name"+pet.tags.name)
+  console.log("pet status"+pet.status)
