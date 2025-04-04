4430ebd6b282bb2151c9d078397040a2559ae27b
ratandeep <ratandeep.ravi@coforge.com>
2025-04-04 13:50:34 +0530

    code

---
+++ petstore.js
@@ -0,0 +1,22 @@
+console.log("Pet Store")
+ 
+let ApiResponse = {
+    "id": 0,
+    "category": {
+        "id": 0,
+        "name": "string"
+    },
+    "name": "doggie",
+    "photoUrls": [
+        "string"
+    ],
+    "tags": [
+        {
+            "id": 0,
+            "name": "string"
+        }
+    ],
+    "status": "available"
+}
+ 
+console.log(ApiResponse)
