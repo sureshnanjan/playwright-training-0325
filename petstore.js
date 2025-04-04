c3a489699fc1c73f8b23723401012e765409ea4b
Shweta Jain - I <Shweta.1.Jain@Coforge.com>
2025-04-04 16:00:23 +0530

    Pet Store

---
+++ petstore.js
@@ -0,0 +1,22 @@
+console.log("Pet Store")
+
+let ApiResponse = {
+    "id": 1,
+    "category": {
+        "id": 123,
+        "name": "Parrot"
+    },
+    "name": "Parrot",
+    "photoUrls": [
+        "../someurl"
+    ],
+    "tags": [
+        {
+            "id": 1,
+            "name": "ParrotCat"
+        }
+    ],
+    "status": "available"
+}
+
+console.log(ApiResponse)
