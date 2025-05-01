651c3864316f115abba9ea69c0507c5449acacbd
Chris Read <chris.read@coforge.com>
2025-05-01 17:37:16 +0100

    commit message

---
+++ tests/challenging_dom_spec.js
@@ -0,0 +1,9 @@
+import {test, expect} from '@playwright/test';
+import { ChallengingDOMPage } from '../pages/challenging_dom_pages.js';
+test("Show alert on right-click on box",() => {
+   ChallengingDOMPage.InvokeOperation();
+
+})
+
+test("Show alert on right-click on box",() => {
+
