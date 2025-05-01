21076f5a478957d5dba25aa7eb560c9f90e31d01
ekanshmeh <mehrotra.em@gmail.com>
2025-05-01 13:46:15 +0100

    changes to sortable fields

--- tests/sortable_data_tables.spec.js
+++ tests/sortable_data_tables.spec.js
@@ -11,3 +11,24 @@ test('Sortable data tables TC', async ({ page }) => {
   await page.locator('#table2').getByRole('cell', { name: 'fbach@yahoo.com' }).click();
   await page.locator('#table2').getByRole('cell', { name: 'http://www.timconway.com' }).click();
 });
+
+
+test('Sortable data tables TC Scenario - 2', async ({ page }) => {
+await page.goto('https://the-internet.herokuapp.com/');
+  await page.getByRole('link', { name: 'Sortable Data Tables' }).click();
+  await page.locator('#table1').getByRole('link', { name: 'edit' }).first().click();
+  await page.locator('#table1').getByRole('link', { name: 'delete' }).first().click();
+  await page.getByRole('heading', { name: 'Example 2' }).click();
+  await page.locator('#table2').getByRole('cell', { name: 'http://www.jsmith.com' }).click();
+  await page.getByRole('cell', { name: '$50.00' }).nth(2).click();
+  await page.locator('#table2').getByRole('cell', { name: 'http://www.timconway.com' }).click();
+  await page.getByText('Class and ID attributes to').click();
+  await page.getByText('Often times when you see a').click();
+  await page.locator('#table2').getByRole('cell', { name: 'http://www.jdoe.com' }).click();
+  await page.locator('#table1').getByText('Last Name').click();
+  await page.locator('#table1').getByText('First Name').click();
+  await page.locator('#table1').getByText('Email').click();
+  await page.locator('#table1').getByRole('cell', { name: 'Due' }).click();
+  await page.locator('#table1').getByText('Web Site').click();
+  await page.locator('#table1').getByText('Action').click();
+});
