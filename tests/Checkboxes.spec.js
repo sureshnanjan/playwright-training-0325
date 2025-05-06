2a47d291f2e75a0bd6d655b296f5c009567f0f24
Shweta Jain - I <Shweta.1.Jain@Coforge.com>
2025-05-06 14:31:20 +0530

    Updating test cases1

--- tests/Checkboxes.spec.js
+++ tests/Checkboxes.spec.js
@@ -6,6 +6,6 @@ test('test', async ({ page }) => {
   await page.getByRole('link', { name: 'Checkboxes' }).click();
   await page.getByRole('heading', { name: 'Checkboxes' }).click();
   await page.getByText('checkbox 1 checkbox').click();
-   await page.getByRole('checkbox').first().check();
-  await page.getByRole('checkbox').nth(1).uncheck();
+  await page.getByRole('checkbox').first().check();
+  //await page.getByRole('checkbox').nth(1).uncheck();
 });
